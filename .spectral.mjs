a08d08b1751c6240b1a91126b84ac17dd41da349
Alex Prokopovich <avient20@gmail.com>
2024-06-18 13:45:30 +0300

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1MDA4OQ/spectral.js?branch_slug=master&token=c15d691f-ed53-4c7d-81e3-91a36674167d";
+export default { extends: ruleset };
